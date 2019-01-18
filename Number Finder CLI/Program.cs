632a913bb49473eec6784afb0b8f3ff08c0cd2a6
CodeChicken <33497381+ChickenCore@users.noreply.github.com>
2019-01-18 18:33:03 -0500

    Added MultipleOf function

--- Number Finder CLI/Program.cs
+++ Number Finder CLI/Program.cs
@@ -10,6 +10,43 @@ namespace Number_Finder_CLI
     {
         static void Main(string[] args)
         {
+            foreach (var item in MultipleOf(GenerateAll(12), 2))
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.Read();
+        }
+
+        static int [] GenerateAll(int size)
+        {
+            int[] outArray = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                outArray[i] = i + 1;
+            }
+            return outArray;
+        }
+        static List<int> MultipleOf (int[] inArray, int multiple)
+        {
+            List<int> outArray = new List<int>();
+
+            foreach (int num in inArray)
+            {
+                double d = ((double)num / (double)multiple);
+                if (d == (int)d)
+                {
+                    Console.WriteLine("Yes! {0} is a multiple of {1}!", num, multiple);
+                    if (outArray != null)
+                    {
+                        outArray.Add(num);
+                    }
+
+                }
+            }
+            
+            return outArray;
         }
     }
 }
