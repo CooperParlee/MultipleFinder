d46fe784359e69cc3ebd479471734961ab893773
CodeChicken <33497381+ChickenCore@users.noreply.github.com>
2019-01-20 23:33:43 -0500

    Added array variants for loads of multiples.

--- Number Finder CLI/Program.cs
+++ Number Finder CLI/Program.cs
@@ -10,10 +10,13 @@ namespace Number_Finder_CLI
     {
         static void Main(string[] args)
         {
-            List<int> range = GenerateAll(1, 20);
-            List<int> outs = MultipleOf(range, 100);
+            List<int> range = GenerateAll(1, 1000);
+            List<int> outs = MultipleArrayOfDifferential(range, new int[] {2, 3, 4, 5, 6}, 1);
 
-            ListItems(outs);
+            Console.WriteLine("There are {0} possibilities in the specified range. Applying next filter", outs.Count);
+            List<int> outs2 = MultipleOf(outs, 7);
+            Console.WriteLine("There are {0} possibilities in this range.", outs2.Count);
+            ListItems(outs2);
             Console.Read();
         }
 
@@ -33,14 +36,15 @@ namespace Number_Finder_CLI
 
             foreach (int num in inArray)
             {
+                Console.Out.WriteLine("Checking if {0}, is a multiple of {1}", num, multiple);
                 double d = ((double)num / (double)multiple);
                 if (d == (int)d)
                 {
-                    if (outArray != null)
-                    {
-                        outArray.Add(num);
-                        break;
-                    }
+                    outArray.Add(num);
+                }
+                else
+                {
+                    Console.WriteLine("Removing number.");
                 }
             }
             if (outArray.Count == 0)
@@ -48,6 +52,41 @@ namespace Number_Finder_CLI
                 Console.WriteLine("There are no multiples in this range.");
             }
             //Console.WriteLine("There are {0} multiples of {1} in the selected range.", outArray.Count, multiple);
+            return outArray;
+        }
+        static List<int> MultipleArrayOf(List<int> inArray, int[] multiples)
+        {
+            List<int> outArray = new List<int>(inArray);
+
+            foreach(int multiple in multiples)
+            {
+                int lastCount = outArray.Count;
+
+                outArray = MultipleOf(outArray, multiple);
+                Console.WriteLine("{0} possible multiples removed. {1} remain.", lastCount - outArray.Count, outArray.Count);
+                if (outArray.Count <= 0)
+                {
+                    Console.WriteLine("There are no valid multiples of said number(s) within this range.");
+                    break;
+                }
+            }
+            return outArray;
+        }
+        static List<int> MultipleArrayOfDifferential(List<int> inArray, int[] multiples, int consistentOffset)
+        {
+            List<int> initArray = new List<int>(inArray);
+            List<int> outArray = new List<int>();
+
+            initArray = MultipleArrayOf(initArray, multiples);
+
+            if(initArray.Count != 0)
+            {
+                foreach (int multiple in initArray)
+                {
+                    outArray.Add(multiple + consistentOffset);
+                }
+            }
+            
             return outArray;
         }
         static List<int>DifferentialFromMultiple(List<int> inArray, int differential, int multiple) // Returns all values that are multiples of "multiple" from inArray, however, applies a differential to their values.
