c7b3cb04adc0f146d54d37d187d2418b5e55ef89
CodeChicken <33497381+ChickenCore@users.noreply.github.com>
2019-01-26 17:10:01 -0500

    Added locking protections and block size adjustments

--- Number Finder CLI/Chunkinator.cs
+++ Number Finder CLI/Chunkinator.cs
@@ -9,6 +9,7 @@ namespace Number_Finder_CLI
     class Chunkinator
     {
         private static int blocksize = 1024;
+        private static bool blocklock = false;
 
         public static List<int> GenerateAll(int min, long max) // Generates every number between the range of min and max.
         {
@@ -23,14 +24,40 @@ namespace Number_Finder_CLI
 
         public static int[] GenerateBlock (int blockID)
         {
-            int[] returnArray = new int[blocksize];
+            int[] m_returnArray = new int[blocksize];
 
             for(int i = 1; i <= blocksize; i++)
             {
                 int startNum = (blockID - 1) * blocksize;
-                returnArray[i - 1] = startNum + i;
+                m_returnArray[i - 1] = startNum + i;
             }
-            return returnArray;
+            return m_returnArray;
+        }
+
+        public static int SetBlockSize(int m_blocksize)
+        {
+            if (!blocklock)
+            {
+                blocksize = m_blocksize;
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public static void SetLockedEnabled(bool m_dolock)
+        {
+            blocklock = m_dolock;
+        }
+
+        public static int GetBlockSize()
+        {
+            return blocksize;
+        }
+        public static bool IsLocked()
+        {
+            return blocklock;
         }
     }
 }
